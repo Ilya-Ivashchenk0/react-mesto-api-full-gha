659413e4812092adfc52c0bc6beedd6b9a58f80c
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-10-02 19:46:44 +0300

    next

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,9 +1,7 @@
-// middlewares/cors.js
-
 const Cors = require('cors')
 
 const cors = {
-  origin: 'https://ilya-mesto.nomoredomainsrocks.ru/', // разрешенные домены
+  origin: 'https://ilya-mesto.nomoredomainsrocks.ru', // разрешенные домены
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'], // разрешенные методы
   credentials: true
 }
